669a6d638e75b6ddc69e7ee6189ff9508bdc97e4
teisha <ahsiet4@yahoo.com>
2020-05-31 19:16:13 -0500

    added JSON output to Texas files

--- txStatRunner.py
+++ txStatRunner.py
@@ -4,10 +4,11 @@ from datetime import datetime
 from IPython.lib.pretty import pprint
 import pickle
 import txarc_config
+from urllib.parse import unquote, urlencode
 
 class Runner:
     def __init__(self):
-        self.stat_pickler = []
+        self.stat_pickler = dict()
         self.current_date_time = datetime.now().strftime("%m-%d-%Y")
 
     def load_from_file(self, filename):
@@ -52,7 +53,43 @@ class Runner:
             viral_stats.append(totals)
         self.stat_pickler.update(dict(viral_antibody_stats=viral_stats))            
 
+    def get_daily_new_cases_by_date(self):
+        response = requests.get(url=txarc_config.DAILY_NEW_CASES_URL, params=txarc_config.daily_new_cases_by_date_params)
+        daily_response = response.json()
+        # print(daily_response)
+        daily_stats = []
+        for feature in daily_response['features']:
+            # print(feature.get('attributes'))
+            totals = feature.get('attributes')
+            totals.update({'DateString': datetime.fromtimestamp(+totals.get('Date')/1000).strftime('%Y-%m-%d') })
+            daily_stats.append(totals)
+        self.stat_pickler.update(dict(daily_new_cases=daily_stats))                
 
+
+    def get_daily_counts_by_county(self):
+        response = requests.get(url=txarc_config.DAILY_COUNTY_COUNTS_URL, params=txarc_config.daily_counts_by_county_params)
+        print(response.url)
+        daily_response = response.json()
+        daily_stats = []
+        for feature in daily_response['features']:
+            print(feature.get('attributes'))
+            totals = feature.get('attributes')
+            totals.update({'DateString': datetime.now().strftime('%Y-%m-%d') })
+            daily_stats.append(totals)
+        self.stat_pickler.update(dict(daily_counts_by_county=daily_stats))         
+
+    def get_county_totals(self):
+        response_total = requests.get(url=txarc_config.TOTAL_COUNTIES_REPORTING_URL, params=txarc_config.total_counties_reporting_params)
+        total_counties = response_total.json()["features"][0]
+        # print(total_counties.get("attributes").get("value"))
+        response_positives = requests.get(url=txarc_config.TOTAL_POSITIVES_URL, params=txarc_config.total_counties_with_positives_params)
+        positive_counties = response_positives.json()["features"][0]
+        # print(positive_counties.get("attributes").get("value"))
+        totals = dict(total_counties=total_counties.get("attributes").get("value"), 
+            positive_counties=positive_counties.get("attributes").get("value"))
+        self.stat_pickler.update(dict(counties=totals))
+        # return (dict(total_counties=total_counties.get("attributes").get("value"), 
+        #     total_positive_counties=positive_counties.get("attributes").get("value"))) 
 
     def write_to_file(self, filename):
         print ('------------------------------------------------------------------')
@@ -60,4 +97,6 @@ class Runner:
         with open(filename, "wb") as handle:
             pickle.dump(self.stat_pickler, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-
+        json_file = filename.replace('.pickle', '.json')
+        with open(json_file, "w") as json_handle:
+            json_handle.writelines(json.dumps(self.stat_pickler)  )          
--- txarc_config.py
+++ txarc_config.py
@@ -1,6 +1,7 @@
 base_url = "https://services5.arcgis.com/ACaLB9ifngzawspq/ArcGIS/rest/services/DSHS_COVID19_Testing_Service/FeatureServer/"
-
-
+base_cases_url = base_url.replace('COVID19_Testing', 'COVID19_Cases')
+          # https://services5.arcgis.com/ACaLB9ifngzawspq/ArcGIS/rest/services/DSHS_COVID19_Testing_Service/FeatureServer/8/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=%2A&orderByFields=Date+asc&resultOffset=0&resultRecordCount=32000&resultType=standard&cacheHint=true  
+          # https://services5.arcgis.com/ACaLB9ifngzawspq/arcgis/rest/services/DSHS_COVID19_Cases_Service/FeatureServer/8/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=Date%20asc&resultOffset=0&resultRecordCount=32000&resultType=standard&cacheHint=true
 current_day_report = "2/query?"   # where=1%3D1&objectIds=&time=&resultType=none&outFields=*&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&sqlFormat=none&f=pjson&token="
 CURRENT_DAY_URL = "" + base_url + current_day_report
 current_day_params = {
@@ -71,12 +72,63 @@ viral_antibody_breakout_by_day_params = {
     'token': ''
 }
 
-daily_new_cases_by_date_url ="8/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=Date%20asc&resultOffset=0&resultRecordCount=32000&resultType=standard&cacheHint=true"
-daily_counts_by_county_url ="0/query?f=json&where=Positive%3C%3E0&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=Positive%20desc&resultOffset=0&resultRecordCount=254&resultType=standard&cacheHint=true"
 
-total_counties_reporting_url ="0/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&outStatistics=%5B%7B%22statisticType%22%3A%22count%22%2C%22onStatisticField%22%3A%22OBJECTID%22%2C%22outStatisticFieldName%22%3A%22value%22%7D%5D&resultType=standard&cacheHint=true"
-total_counties_with_positives_url ="0/query?f=json&where=Positive%3C%3E0&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&outStatistics=%5B%7B%22statisticType%22%3A%22count%22%2C%22onStatisticField%22%3A%22OBJECTID%22%2C%22outStatisticFieldName%22%3A%22value%22%7D%5D&resultType=standard&cacheHint=true"
+daily_new_cases_by_date_url ="8/query?"   # f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=Date%20asc&resultOffset=0&resultRecordCount=32000&resultType=standard&cacheHint=true"
+DAILY_NEW_CASES_URL = base_cases_url + daily_new_cases_by_date_url
+daily_new_cases_by_date_params = {
+    'f': 'json',
+    'where': '1=1',
+    'returnGeometry': 'false',
+    'spatialRel': 'esriSpatialRelIntersects',
+    'outFields':'*',
+    'orderByFields': 'Date asc',
+    'resultOffset': '0',
+    'resultRecordCount':'32000',
+    'resultType': 'standard',
+    'cacheHint': 'true'
+}
 
+
+daily_counts_by_county_url ="0/query?"   # f=json&where=Positive%3C%3E0&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=Positive%20desc&resultOffset=0&resultRecordCount=254&resultType=standard&cacheHint=true"
+DAILY_COUNTY_COUNTS_URL = base_cases_url + daily_counts_by_county_url
+daily_counts_by_county_params = {
+    'f': 'json',
+    'where': 'Positive>0',
+    'returnGeometry': 'false',
+    'spatialRel': 'esriSpatialRelIntersects',
+    'outFields':'*',
+    'orderByFields': 'Positive desc',
+    'resultOffset': '0',
+    'resultRecordCount':'500',
+    'resultType': 'standard',
+    'cacheHint': 'true'
+}
+
+total_counties_reporting_url ="0/query?" # f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&outStatistics=%5B%7B%22statisticType%22%3A%22count%22%2C%22onStatisticField%22%3A%22OBJECTID%22%2C%22outStatisticFieldName%22%3A%22value%22%7D%5D&resultType=standard&cacheHint=true"
+TOTAL_COUNTIES_REPORTING_URL = base_cases_url + total_counties_reporting_url
+total_counties_reporting_params = {
+    'f': 'json',
+    'where': '1=1',
+    'returnGeometry': 'false',
+    'spatialRel': 'esriSpatialRelIntersects',
+    'outFields':'*',
+    'outStatistics':'[{"statisticType":"count","onStatisticField":"OBJECTID","outStatisticFieldName":"value"}]',
+    'resultType': 'standard',
+    'cacheHint': 'true'
+}
+
+total_counties_with_positives_url ="0/query?" # f=json&where=Positive%3C%3E0&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&outStatistics=%5B%7B%22statisticType%22%3A%22count%22%2C%22onStatisticField%22%3A%22OBJECTID%22%2C%22outStatisticFieldName%22%3A%22value%22%7D%5D&resultType=standard&cacheHint=true"
+TOTAL_POSITIVES_URL = base_cases_url + total_counties_with_positives_url
+total_counties_with_positives_params = {
+    'f': 'json',
+    'where': 'Positive>0',
+    'returnGeometry': 'false',
+    'spatialRel': 'esriSpatialRelIntersects',
+    'outFields':'*',
+    'outStatistics':'[{"statisticType":"count","onStatisticField":"OBJECTID","outStatisticFieldName":"value"}]',
+    'resultType': 'standard',
+    'cacheHint': 'true'
+}
 
 
 
