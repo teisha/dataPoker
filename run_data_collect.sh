d2fb61cf1f26522299f33c7382e2785bd20d9c0c
Teisha Buford <ahsiet4@yahoo.com>
2022-12-17 18:54:54 -0600

    get this running on new computer

---
+++ run_data_collect.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+FILE="outfile.txt"
+FILE=${FILE%.*}_`date +%d%b%y`.${FILE#*.}
+echo $FILE
+
+venv/bin/python main.py >> ./out/$FILE
+# venv/bin/python data_reporters/texas_reporter.py
+
+
+
+# pyenv virtualenv 3.10.8 datapoker
+# pyenv uninstall datapoker
+# ls ~/.pyenv/versions/
+# ln -s ~/.pyenv/versions/3.10.8/envs/datapoker venv  -->  Where venv lives from pyenv-virtualenv
+
+# venv/bin/python -m pip install requests
+# venv/bin/python -m pip install ipython
+# venv/bin/python -m pip install firebase-admin                  
+# venv/bin/python -m pip install beautifulsoup4 
+# venv/bin/python -m pip install pandas 
+
+# venv/bin/python -m pip freeze > requirements.txt  
+# venv/bin/python -m pip uninstall -r requirements.txt -y
+
+# 
+
